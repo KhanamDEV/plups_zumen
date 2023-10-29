d9be9226d903672ebf276ff5425738d326456f92
AndrewNguyen <khanamdev@gmail.com>
2023-10-29 08:04:29 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function (){
     AOS.init();
-
+    $(".open-nav-mobile").click(function (){
+        $("#header-main #nav-mobile").addClass('open')
+    });
+    $(".close-menu").click(function (){
+        $("#header-main #nav-mobile").removeClass('open')
+    });
 })
